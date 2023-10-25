13679ae8835b9a6f959a0d2852bd1286df411550
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-10-25 18:08:52 +0900

    finish create bottomnavigationbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:kitcamp2023/calender.dart';
+import 'package:kitcamp2023/role.dart';
+import 'package:kitcamp2023/talk_box.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,63 +11,79 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // ignore: library_private_types_in_public_api
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  var _selectedTab = _SelectedTab.home;
 
-  void _incrementCounter() {
+  void _handleIndexChanged(int i) {
     setState(() {
-      _counter++;
+      _selectedTab = _SelectedTab.values[i];
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    var screen = [const Calender(), const Role(), const TalkBox()];
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      extendBody: true,
+      body: screen[_selectedTab.index],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: DotNavigationBar(
+          margin: const EdgeInsets.only(left: 5, right: 5),
+          currentIndex: _SelectedTab.values.indexOf(_selectedTab),
+          dotIndicatorColor: const Color(0xFF333A47),
+          backgroundColor: Colors.teal[200],
+          unselectedItemColor: Colors.grey[300],
+          splashBorderRadius: 50,
+          // enableFloatingNavBar: false,
+          onTap: _handleIndexChanged,
+          items: [
+            /// Home
+            DotNavigationBarItem(
+              icon: Icon(Icons.home),
+              selectedColor: const Color(0xFF333A47),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+            /// Likes
+            DotNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              selectedColor: const Color(0xFF333A47),
+            ),
+
+            /// Search
+            DotNavigationBarItem(
+              icon: Icon(Icons.search),
+              selectedColor: const Color(0xFF333A47),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
+
+// ignore: constant_identifier_names
+enum _SelectedTab { home, favorite, search }
---
+++ lib/talk_box.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TalkBox extends StatelessWidget {
+  const TalkBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("talk box"),
+    );
+  }
+}
